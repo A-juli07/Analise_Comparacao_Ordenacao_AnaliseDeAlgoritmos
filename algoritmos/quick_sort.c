61629527d05647fc759f413f7c46e0e67b04cece
Gabriel Peixoto Menezes da Costa <100876842+gabiel98@users.noreply.github.com>
2025-05-26 10:36:16 -0400

    Update quick_sort.c
    Mudando o pivo para uma escolha melhor e nao sempre o ultimo elemento como o pivo

--- algoritmos/quick_sort.c
+++ algoritmos/quick_sort.c
@@ -1,21 +1,45 @@
+int median_of_three(int *arr, int low, int high) {
+    int mid = low + (high - low) / 2;
+    if (arr[low] > arr[mid]) {
+        int tmp = arr[low]; arr[low] = arr[mid]; arr[mid] = tmp;
+    }
+    if (arr[mid] > arr[high]) {
+        int tmp = arr[mid]; arr[mid] = arr[high]; arr[high] = tmp;
+    }
+    if (arr[low] > arr[mid]) {
+        int tmp = arr[low]; arr[low] = arr[mid]; arr[mid] = tmp;
+    }
+    return mid; 
+}
+
 int partition(int *arr, int low, int high, long long *comparacoes) {
-    int pivot = arr[high], i = low - 1;
+    int pivot_idx = median_of_three(arr, low, high);
+    int tmp = arr[pivot_idx]; arr[pivot_idx] = arr[high]; arr[high] = tmp;
+
+    int pivot = arr[high];
+    int i = low - 1;
 
     for (int j = low; j < high; j++) {
         (*comparacoes)++;
         if (arr[j] <= pivot) {
             i++;
-            int tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
+            tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
         }
     }
-    int tmp = arr[i+1]; arr[i+1] = arr[high]; arr[high] = tmp;
+    tmp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = tmp;
     return i + 1;
 }
 
 void quick_sort(int *arr, int low, int high, long long *comparacoes) {
-    if (low < high) {
+    while (low < high) {
         int pi = partition(arr, low, high, comparacoes);
-        quick_sort(arr, low, pi - 1, comparacoes);
-        quick_sort(arr, pi + 1, high, comparacoes);
+
+        if (pi - low < high - pi) {
+            quick_sort(arr, low, pi - 1, comparacoes);
+            low = pi + 1;
+        } else {
+            quick_sort(arr, pi + 1, high, comparacoes);
+            high = pi - 1;
+        }
     }
 }
